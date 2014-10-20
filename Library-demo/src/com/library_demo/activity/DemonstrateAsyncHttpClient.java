4519474afc06051ed2da6d1cc897c63ad6311d5f
Lushuwei(Albert) <albertxiaoyu@163.com>
2014-10-20 11:25:59 +0800

    去除findViewById
    使用butterknife这个轻量级View注入框架来去除各种findViewById。
    在获取View的引用时，无需使用butterknife。

---
+++ Library-demo/src/com/library_demo/activity/DemonstrateAsyncHttpClient.java
@@ -0,0 +1,20 @@
+/*
+*  @author LuShuWei  E-mail:albertxiaoyu@163.com
+*  创建时间 2014-10-20
+*/
+
+package com.library_demo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemonstrateAsyncHttpClient extends Activity {
+	
+	
+	public void onCreate(Bundle state)
+	{
+		
+	}
+
+}
+
