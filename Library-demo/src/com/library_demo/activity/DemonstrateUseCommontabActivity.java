40954045d3a5c4afe81286e074b677887303cdd1
Lushuwei(Albert) <albertxiaoyu@163.com>
2014-10-31 16:11:08 +0800

    add tabBg to show tab selected state changing
    use the [android:state_enabled], [android:state_selected]
    properties to distinguish the tab selected state.

--- Library-demo/src/com/library_demo/activity/DemonstrateUseCommontabActivity.java
+++ Library-demo/src/com/library_demo/activity/DemonstrateUseCommontabActivity.java
@@ -1,7 +1,7 @@
 /** 
  * @author LuShuWei E-mail:albertxiaoyu@163.com 
- * @version 创建时间：2014-10-30 下午8:23:29 
- * 类说明 
+ * @version 鍒涘缓鏃堕棿锛�2014-10-30 涓嬪崍8:23:29 
+ * 绫昏鏄� 
  */
 
 package com.library_demo.activity;
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.androidlibrary.activity.CommonTabActivity;
 import com.library_demo.*;
 
@@ -19,7 +21,7 @@ public class DemonstrateUseCommontabActivity extends CommonTabActivity {
 	
 	public void OnCreate(Bundle state) {
 		super.onCreate(state);
-		initalTabItems();
+		
 	}
 	
 	private void initalTabItems()
@@ -29,10 +31,14 @@ public class DemonstrateUseCommontabActivity extends CommonTabActivity {
 		Intent threeIntent = new Intent(this,DemonstrateCustomDialogActivity.class);
 		Intent fourIntent = new Intent(this,DemonstrateUseLibActivity.class);
 		
-		TabItem one = new TabItem(R.drawable.ic_launcher,"SoapRequest",oneIntent);
-		TabItem two = new TabItem(R.drawable.ic_launcher,"ValueUtil",twoIntent);
-		TabItem three = new TabItem(R.drawable.ic_launcher,"CustomDialog",threeIntent);
-		TabItem four = new TabItem(R.drawable.ic_launcher,"UseLib",fourIntent);
+		TabItem one = new TabItem("SoapRequest",oneIntent,"soapRequest");
+		one.setTabBg(R.drawable.tab_bg);
+		TabItem two = new TabItem("ValueUtil",twoIntent,"valueUtil");
+		two.setTabBg(R.drawable.tab_bg);
+		TabItem three = new TabItem("CustomDialog",threeIntent,"customDialog");
+		three.setTabBg(R.drawable.tab_bg);
+		TabItem four = new TabItem("UseLib",fourIntent,"useLib");
+		four.setTabBg(R.drawable.tab_bg);
 		
 		items.add(one);
 		items.add(two);
@@ -45,6 +51,21 @@ public class DemonstrateUseCommontabActivity extends CommonTabActivity {
 	protected ArrayList<TabItem> getTabItems() {
 		// TODO Auto-generated method stub
 		return items;
+	}
+
+	@Override
+	protected void prepareDatas() {
+		// TODO Auto-generated method stub
+		initalTabItems();
+	}
+
+	@Override
+	protected void setTabView(TextView tabView,int position) {
+		// TODO Auto-generated method stub
+		TabItem one = items.get(position);
+		tabView.setText(one.getTabName());
+		tabView.setBackgroundResource(one.getTabBg());
+		
 	}
 
 
---
+++ Library-demo/src/com/library_demo/activity/TestTabActivity.java
@@ -0,0 +1,7 @@
+package com.library_demo.activity;
+
+import android.app.Activity;
+
+public class TestTabActivity extends Activity {
+
+}
